4be90aa4046dc28484eb767e798bebdd5ede2130
RasKids <RasKids@macs-MacBook-Pro-2.local>
2021-04-20 09:12:42 -0600

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConversationComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
