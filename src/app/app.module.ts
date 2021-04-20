d69dc82659db15ac1f9fbb60ba268cfe584ff41b
dougalcaleb <dougalcaleb@gmail.com>
2021-04-20 09:18:00 -0600

    add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { ChatListComponent } from './pages/chat-list/chat-list.component';
+import { ConversationComponent } from './pages/conversation/conversation.component';
+import { UserComponent } from './pages/user/user.component';
+import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChatListComponent,
+    ConversationComponent,
+    UserComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
