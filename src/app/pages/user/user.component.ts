8392da4ae3559645f60acfb6809429f7a9b7564a
RasKids <RasKids@macs-MacBook-Pro-2.local>
2021-04-29 10:17:21 -0600

    changing data

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { StoreService } from 'src/app/services/store.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
-  display = false;
-  toggle() {
-    if (this.display == false) {
-      this.display = true;
-    } else {
-      this.display = false;
+    constructor(
+        public Store: StoreService,
+        private firebase: FirebaseService
+    ) {}
+    display = false;
+    toggle() {
+        if (this.display == false) {
+            this.display = true;
+        } else {
+            this.display = false;
+        }
     }
-  }
 
-  constructor(public Store: StoreService) { }
+    updateInfo() {
+        this.toggle();
+        this.firebase.updateUser( this.Store.activeUser_Firebase, this.Store.activeUser_Firebase.color, 'changeColor' );
+        this.firebase.updateUser( this.Store.activeUser_Firebase, this.Store.activeUser_Firebase.username, 'changeUsername' );
+        this.firebase.updateUser( this.Store.activeUser_Firebase, this.Store.activeUser_Firebase.displayName, 'changeDisplayName' );
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 }
