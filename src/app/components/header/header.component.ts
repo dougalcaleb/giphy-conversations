9f211bb386def589ef2fa39e29662553c815e5a4
dougalcaleb <dougalcaleb@gmail.com>
2021-05-03 10:45:57 -0600

    add header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public Store: StoreService) { }
 
   ngOnInit(): void {
   }
